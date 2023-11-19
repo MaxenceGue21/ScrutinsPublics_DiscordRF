3816423e9dcaf6968c9bb0b1560684c1f986063a
Maxence <107473548+MaxenceGue21@users.noreply.github.com>
2023-11-19 15:04:21 +0100

    ✍ script.js updated
    🟠 Changement du système de fonctionnement [-> création d'une fonction calculate() appelée au clique du bouton submit et qui effectue l'intégralité du processus (non modifié) 🔸].

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-// * Ciblage du bouton "Valider" et ajout d'un événement lors du clique.
-let button = document.getElementById("submitButton");
-button.addEventListener("click", calculation);
-
-// * Fonction "calculation" qui est appelée lors du clique sur le bouton "Valider".
-function calculation() {
+// * Fonction "calculate" qui est appelée lors du clique sur le bouton "Valider".
+function calculate() {
   // Définition des variables qui sauvegardent les valeurs mises dans les inputs du formulaire.
   let votes_pour = document.getElementById("pour").value;
   let votes_contre = document.getElementById("contre").value;
@@ -14,41 +10,68 @@ function calculation() {
   let exprimes = Number(votes_pour) + Number(votes_contre);
   let majorite = Number(exprimes) / 2;
 
-  // Condition sélectionnant le texte qui apparaîtra en fonction des nombres obtenus respectivement dans les variables "votes_pour" et "votes_contre".
-  if (votes_pour > votes_contre) {
-    var result = "L'Assemblée nationale a adopté.";
-  } else if (votes_contre >= votes_pour) {
-    var result2 = "L'Assemblée nationale n'a pas adopté.";
-  }
-
   // Modification du résultat de la majorité absolue selon que le résultat obtenu soit un nombre entier ou un nombre décimal.
   if (Number.isInteger(majorite)) {
     let majorite2 = Number(majorite) + 1;
-    alert(
-      "RÉSULTATS DU SCRUTIN :\n\n - Votants : " +
-        votants +
-        "\n - Exprimés : " +
-        exprimes +
-        "\n - Majorité absolue : " +
-        majorite2 +
-        "\n\n" +
-        result +
-        "\n" +
-        result2
-    );
+    if (votes_pour > votes_contre) {
+      alert(
+        "RÉSULTATS DU SCRUTIN :\n\n - Votants : " +
+          votants +
+          "\n - Exprimés : " +
+          exprimes +
+          "\n - Majorité absolue : " +
+          majorite2 +
+          "\n\n - POUR : " +
+          votes_pour +
+          "\n - CONTRE : " +
+          votes_contre +
+          "\n\n L'Assemblée nationale a adopté."
+      );
+    } else if (votes_contre >= votes_pour) {
+      alert(
+        "RÉSULTATS DU SCRUTIN :\n\n - Votants : " +
+          votants +
+          "\n - Exprimés : " +
+          exprimes +
+          "\n - Majorité absolue : " +
+          majorite2 +
+          "\n\n - POUR : " +
+          votes_pour +
+          "\n - CONTRE : " +
+          votes_contre +
+          "\n\n L'Assemblée nationale n'a pas adopté."
+      );
+    }
   } else {
     let majorite3 = Number(majorite) + 0.5;
-    alert(
-      "RÉSULTATS DU SCRUTIN :\n\n - Votants : " +
-        votants +
-        "\n - Exprimés : " +
-        exprimes +
-        "\n - Majorité absolue : " +
-        majorite3 +
-        "\n\n" +
-        result +
-        "\n" +
-        result2
-    );
+    if (votes_pour > votes_contre) {
+      alert(
+        "RÉSULTATS DU SCRUTIN :\n\n - Votants : " +
+          votants +
+          "\n - Exprimés : " +
+          exprimes +
+          "\n - Majorité absolue : " +
+          majorite3 +
+          "\n\n - POUR : " +
+          votes_pour +
+          "\n - CONTRE : " +
+          votes_contre +
+          "\n\n L'Assemblée nationale a adopté."
+      );
+    } else if (votes_contre >= votes_pour) {
+      alert(
+        "RÉSULTATS DU SCRUTIN :\n\n - Votants : " +
+          votants +
+          "\n - Exprimés : " +
+          exprimes +
+          "\n - Majorité absolue : " +
+          majorite3 +
+          "\n\n - POUR : " +
+          votes_pour +
+          "\n - CONTRE : " +
+          votes_contre +
+          "\n\n L'Assemblée nationale n'a pas adopté."
+      );
+    }
   }
 }
